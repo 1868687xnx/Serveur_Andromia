cbadac620810ab857d0e8adeb6193dbe8013fb98
1868687xnx <1868687@cstj.qc.ca>
2025-10-01 11:07:17 -0400

    server semi fonctionel

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import cors from 'cors';
+import express from 'express';
+
+import database from './core/database.js';
+import errors from './middlewares/errors.js';
+
+const app = express();
+
+database();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/status', (req, res) => { res.status(200).end(); });
+app.head('/status', (req, res) => { res.status(200).end(); });
+
+
+app.use(errors);
+
+export default app;
